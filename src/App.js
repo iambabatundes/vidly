705af099fa8f3d0abbe4cc809f4c5f18dd76e137
Emmanuel Babatunde <iambabatundes@gmail.com>
2023-01-23 22:05:51 -0800

    creating other routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from "./logo.svg";
+import React from "react";
+import { Route, redirect } from "react-router-dom";
 import "./App.css";
+import Customers from "./components/customers";
 import Movies from "./components/movies";
+import Rentals from "./components/rentals";
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Route path="/movies" element={Movies} />
+      <Route path="/customers" element={Customers} />
+      <Route path="/rentals" element={Rentals} />
+      <Route path="/" element={Movies} />
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
@@ -9,7 +10,9 @@ import "font-awesome/css/font-awesome.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
